3e2106b9ae08030e427bddb16b5d9543719771b9
lzw1231 <luozhw.bit@gmail.com>
2026-09-03 09:46:18 +0800

    111

--- src/test_so/src/test_so.cpp
+++ src/test_so/src/test_so.cpp
@@ -7,6 +7,14 @@
 
 int main() {
     int major, minor, release, revision;
-    dhdGetSDKVersion (&major, &minor, &release, &revision);
+    dhdGetSDKVersion(&major, &minor, &release, &revision);
     printf("Force Dimension SDK, version %i.%i.%i", major, minor, release);
+
+    SMS_STS sms_sts_;
+
+    char id = 11;
+
+    sms_sts_.begin(1000000, &id);
+
+    sms_sts_.
 }
